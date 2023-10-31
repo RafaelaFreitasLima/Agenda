1a24648ba2ab653cfb8de85a687bc876beedf546
Rafaela Lima <rafaelafreitaslima333@gmail.com>
2023-10-30 21:53:35 -0300

    inclusão do dado civil

--- AgendaApp/models.py
+++ AgendaApp/models.py
@@ -3,6 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Contato(models.Model):
+
+# Opcoes do campo estado civil
+# primeiro valor da tupla é o que vai no banco e o segundo o que vai na tela
+
+    ESTADO_CIVIS = [
+        ('S','Solteiro'), 
+        ('C','Casado'), 
+        ('D','Divorciado'), 
+        ('V','Viúvo')
+        ]
+
     nome = models.CharField(max_length=100)
     apelido = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=100)
@@ -14,6 +25,7 @@ class Contato(models.Model):
     bairro = models.CharField(max_length=100)
     cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=50)
+    estado_civil = models.CharField(max_length=1, choices=ESTADO_CIVIS, null=True)
 
 def __str__(self):
     return self.nome
